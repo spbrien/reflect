658b2be13b662d911baefbdec8e1f7eb2c7a6afb
Steven Brien <sbrien@hlkagency.com>
2017-03-03 13:44:25 -0600

    Updating settings file for clarity

--- app/settings.py
+++ app/settings.py
@@ -1,20 +1,29 @@
 # Using Scotchbox with a wordpress database for testing
 # Get one here --> https://box.scotch.io/
 # Even though (and maybe because) wordpress databases suck
+# ----------------------------------
 DB_STRING = 'mysql+mysqldb://root:root@192.168.33.133:3306/scotchbox'
 
+
 # Auth Server
+# ----------------------------------
 AUTHENTICATION_SERVER = ''
 AUTHENTICATION_SCOPE = ''
+
 
 # Default User -- THIS OPTION SHOULD ONLY BE SET FOR DEVELOPMENT
 # Authentication class should be subclassed and set up
 # to use an outside Auth server
+# ----------------------------------
 ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = 'test'
+# ADMIN_USERNAME = None
+# ADMIN_PASSWORD = None
+
 
 # Expose endpoints (tables) by Role for each HTTP verb
 # TODO: Add column restriction functionality as well
+# ----------------------------------
 AUTHENTICATION_SETTINGS = {
     'GET': {
         'wp_posts': {
@@ -28,5 +37,7 @@ AUTHENTICATION_SETTINGS = {
     'DELETE': {}
 }
 
+
 # Field to match when fetching single items
+# ----------------------------------
 ID_FIELD = 'ID'
